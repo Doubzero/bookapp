80ef8d85aec43b6e3b1ee6b4f74418709de54669
Doubzero <131899921+Doubzero@users.noreply.github.com>
2023-09-29 11:35:21 -0400

    Created book object and add book function.

---
+++ app.js
@@ -0,0 +1,12 @@
+let library = [];
+
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.publicationYear = publicationYear;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+  let newBook = new Book(title, author, pages, isRead);
+}
