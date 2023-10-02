176b740d8b58320326dd930777fae185d66d2765
Doubzero <131899921+Doubzero@users.noreply.github.com>
2023-10-02 13:22:35 -0400

    Added function to apply form input to table.

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ function Book(title, author, pages, isRead) {
 function addBookToLibrary(title, author, pages, isRead) {
   let newBook = new Book(title, author, pages, isRead);
   library.push(newBook);
+  addBooksToTable();
 }
 function addBookFromForm(e) {
+  e.preventDefault();
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = parseInt(document.getElementById("pages").value);
@@ -28,5 +30,30 @@ function addBookFromForm(e) {
   document.getElementById("author").value = "";
   document.getElementById("pages").value = "";
   document.getElementById("isRead").checked = false;
-  e.preventDefault(); //prevents form from submitting.
+}
+
+function addBooksToTable() {
+  const tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
+  // Clear the existing content in the table body
+
+  for (let i = 0; i < library.length; i += 1) {
+    const book = library[i];
+
+    if (!book.title.trim() || !book.author.trim()) {
+      continue; // Skip adding books with empty title or author
+    }
+
+    const bookRow = document.createElement("tr");
+
+    // Create table cells for book details
+    const bookTitle = document.createElement("td");
+    bookTitle.textContent = book.title;
+    const bookAuthor = document.createElement("td");
+    bookAuthor.textContent = book.author;
+
+    bookRow.appendChild(bookTitle);
+    bookRow.appendChild(bookAuthor);
+    tableBody.appendChild(bookRow);
+  }
 }
